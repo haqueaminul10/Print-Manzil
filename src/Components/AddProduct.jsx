a1bc1a711fb60b0f89c609865a32f724f960b3b4
haqueaminul10 <haqueaminul10100@gmail.com>
2025-01-10 01:47:34 +0600

    product list todo

--- src/Components/AddProduct.jsx
+++ src/Components/AddProduct.jsx
@@ -10,6 +10,46 @@ const AddProduct = () => {
   const [uploadedLogo, setUploadedLogo] = useState(null);
   const [logoSize, setLogoSize] = useState({ width: 100, height: 100 });
   const tshirtRef = useRef();
+  const [isDragging, setIsDragging] = useState(false);
+  const fileInputRef = useRef(null);
+  const selectFiles = () => fileInputRef.current.click();
+
+  const onFileSelect = (event) => {
+    const files = Array.from(event.target.files);
+    const validImages = files
+      .filter((file) => file.type.startsWith('image/'))
+      .filter((file) => !images.some((image) => image.name === file.name));
+
+    const newImages = validImages.map((file) => ({
+      name: file.name,
+      url: URL.createObjectURL(file),
+    }));
+
+    setImages((prevImages) => [...prevImages, ...newImages]);
+  };
+
+  const onDragLeave = () => setIsDragging(false);
+  const onDragOver = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const onDrop = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+
+    const files = Array.from(event.dataTransfer.files);
+    const validImages = files
+      .filter((file) => file.type.startsWith('image/'))
+      .filter((file) => !images.some((image) => image.name === file.name));
+
+    const newImages = validImages.map((file) => ({
+      name: file.name,
+      url: URL.createObjectURL(file),
+    }));
+
+    setImages((prevImages) => [...prevImages, ...newImages]);
+  };
 
   const handleLogoUpload = (e) => {
     const file = e.target.files[0];
@@ -66,14 +106,69 @@ const AddProduct = () => {
         </div>
 
         <div className='controls-section'>
-          <div className='upload-input-container'>
+          {/* <div className='upload-input-container'>
             <input
               type='file'
               accept='image/*'
               onChange={handleLogoUpload}
               className='upload-input'
             />
-          </div>
+          </div> */}
+
+          <section className='product-photo'>
+            <div
+              className='drag-area'
+              onDragOver={onDragOver}
+              onDragLeave={onDragLeave}
+              onDrop={onDrop}
+            >
+              {isDragging ? (
+                <span className='select'>Drop image here</span>
+              ) : (
+                <div>
+                  <div className='upload-icon'></div>
+                  <div>
+                    <span className='drag-drop-text'>
+                      {' '}
+                      Drag & Drop images here , or{'  '}
+                    </span>
+
+                    <span
+                      className='select clicktobrowse'
+                      onClick={selectFiles}
+                    >
+                      Click To Browse
+                    </span>
+                  </div>
+                </div>
+              )}
+
+              <input
+                type='file'
+                name='file'
+                multiple
+                ref={fileInputRef}
+                // onChange={onFileSelect}
+                onChange={handleLogoUpload}
+                accept='image/*'
+              />
+            </div>
+            {/* <div className='container'>
+              {images.map((image, index) => (
+                <div className='image' key={index}>
+                  <span
+                    className='delete'
+                    onClick={() => deleteImage(index)}
+                    aria-label='Delete image'
+                  >
+                    &times;
+                  </span>
+                  <img src={image.url} alt={image.name} />
+                </div>
+              ))}
+            </div> */}
+          </section>
+
           <button onClick={handleSaveDesign} className='submit-button'>
             Submit Design
           </button>
